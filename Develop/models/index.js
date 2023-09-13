bcfeab53d169de50a523ef1fb6e1feca82d837dd
dmmontoto <dmontoto@ucsd.edu>
2023-09-13 11:24:46 -0700

    added rest of models and relations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // This should match the column name in the Product table that references Category
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // This should match the column name in the Product table that references Category
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id', // This should match the column name in the ProductTag table that references Product
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id', // This should match the column name in the ProductTag table that references Tag
+});
 
 module.exports = {
   Product,
